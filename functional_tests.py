7349068e6e183871b4a7d21c7114c1255068d380
dims <8914843.pc@gmail.com>
2022-11-01 17:53:57 +0300

    Функциональный тест. Начало

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,33 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class VisitorTest(unittest.TestCase):
+    '''тест посетителя'''
 
-assert 'The install worked successfully! Congratulations!' in browser.title
+    def setUp(self):
+        '''установка'''
+        self.browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+    def tearDown(self):
+        '''демонтаж'''
+        self.browser.quit()
+
+    def test_can_open_table(self):
+        '''тест: можно найти продукт и посмотреть'''
+        # Открываем домашнюю страницу
+        self.browser.get('http://localhost:8000')
+
+        # В заголовке и шапке страницы указано, что здесь предоставляется инфа о пит ценности пищи
+        self.assertIn('Питательная ценность продуктов', self.browser.title)
+        self.fail('Закончить тест')
+
+        # Сразу предлагается выбрать из выпадающего списка категорию продукта
+        # Поле находится под надписью "Выберите категорию продукта"
+        # Наводим курсор на поле с продуктами и выпадает список категорий продуктов
+        # Щелкаем на выбранную категорию и попадаем на страницу со списком продуктов выбранной категории
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
