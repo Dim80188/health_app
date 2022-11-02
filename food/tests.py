f14195931765e145abb1717df642cbe8d3bddcb4
dims <8914843.pc@gmail.com>
2022-11-02 09:54:01 +0300

    Возвращаем минимальный HTML

--- food/tests.py
+++ food/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from .views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 
@@ -13,3 +14,13 @@ class HomePageTest(TestCase):
 
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        '''тест: домашняя страница возвращает правильный html'''
+
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Питательная ценность продуктов</title>', html)
+        self.assertTrue(html.endswith('</html>'))
