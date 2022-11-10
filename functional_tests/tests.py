bda5cf13ea73b2b0c65b3fbfd7a39750561798af
dims <8914843.pc@gmail.com>
2022-11-10 06:39:04 +0300

    Начало стилевого оформления

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -123,6 +123,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Говядина 100 гр', page_text)
         self.assertIn('Молоко 1 л', page_text)
 
+    def test_layout_and_styling(self):
+        '''тест макета и стилевого оформления'''
+        # пользователь открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # он замечает, что поле ввода аккуратно центрировано
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 498, delta = 10
+        )
+
+        # он начинает новый список и видит, что поле ввода там тоже
+        # аккуратно центрировано
+
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 498, delta=10
+        )
 
 
 if __name__ == '__main__':
