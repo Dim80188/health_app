cf5fd4ac4b21b9c29238e4816da7c7f7aa567cf7
dims <8914843.pc@gmail.com>
2022-11-22 10:18:53 +0300

    Преобразование ФТ

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -67,6 +67,32 @@ class ItemValidationTest(FunctionalTest):
 
         # он видит полезное сообщение об ошибке
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
+            self.get_error_element().text,
             "You've already got this in your list"
         ))
+
+    def test_error_messages_are_cleared_on_input(self):
+        '''тест: сообщения об ошибках очищаются при вводе'''
+        # Пользователь начинает список и вызывает ошибку валидации
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # Он начинает набирать в поле ввода, чтобы очистить ошибку
+        self.get_item_input_box().send_keys('a')
+
+        # Сообщение об ошибке исчезает
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
+        ))
+
+    def get_error_element(self):
+        '''получить элемент с ошибкой'''
+        return self.browser.find_element(By.CSS_SELECTOR, '.has-error')
